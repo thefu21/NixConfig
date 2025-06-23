0c5a0a3662191767105a77af12804a5ac522f4c1
Theodor Fumics <theodor.fumics@gmx.net>
2025-06-23 11:17:15 +0200

    feat: add mysql options

--- nixosModules/features/mariaDB-Apache.nix
+++ nixosModules/features/mariaDB-Apache.nix
@@ -6,6 +6,11 @@
   services.mysql.enable = true;
   services.mysql.package = pkgs.mariadb;
   systemd.services.mysql.wantedBy = lib.mkForce [];
+  services.mysql.settings = {
+    mysqld = {
+      sql_mode = "STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION";
+    };
+  };
 
   services.httpd.enablePHP = true;
   services.httpd.enable = true;
